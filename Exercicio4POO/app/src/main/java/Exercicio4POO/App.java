2f2991124dfd58fa7481ef51d9c899ca9b78a573
Verônica Freitas Santos <verofreit@gmail.com>
2023-08-21 14:55:25 -0300

    primeira tela com java

--- Exercicio4POO/app/src/main/java/Exercicio4POO/App.java
+++ Exercicio4POO/app/src/main/java/Exercicio4POO/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         
         Invoice inv = new Invoice(1, "Mousepad",
-                -10, 50.0f);
+                10, 50.0f);
         
         System.out.println("Valor total = " + inv.getInvoiceAmount());
         
